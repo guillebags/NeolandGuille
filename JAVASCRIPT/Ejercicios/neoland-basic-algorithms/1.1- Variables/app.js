1a96e63a6cdd6fa84575febc96b57bb6dfb4369c
guillebags <guillebags@gmail.com>
2023-07-04 15:50:06 +0200

    1.1-Variables

---
+++ JAVASCRIPT/Ejercicios/neoland-basic-algorithms/1.1- Variables/app.js
@@ -0,0 +1,38 @@
+//! --- Variables
+
+//1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+
+let myFavoriteHero = "Hulk";
+
+//1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+
+let x = 50;
+
+//1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+
+let h = 5,
+  y = 10;
+
+//1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+
+let z = h + y;
+
+console.log(myFavoriteHero, x, h, y, z);
+
+//! --- Variables Avanzadas
+
+//1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
+
+const character = { name: "Jack Sparrow", age: 10 };
+
+character.age = 25;
+
+console.log(character);
+
+//1.2 Declara 3 variables con los nombres y valores siguientes
+
+firstName = "Jon";
+lastName = "Snow";
+age = 24;
+
+console.log(`Soy ${firstName}`);
