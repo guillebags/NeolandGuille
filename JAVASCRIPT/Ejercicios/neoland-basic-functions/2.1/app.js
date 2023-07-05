6d25c2b4d7217b8061a730d305522d7b0e2b6af2
guillebags <guillebags@gmail.com>
2023-07-05 17:52:17 +0200

    Buscar el máximo

---
+++ JAVASCRIPT/Ejercicios/neoland-basic-functions/2.1/app.js
@@ -0,0 +1,10 @@
+//! Iteración #1: Buscar el máximo
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne, numberTwo) {
+  return Math.max(numberOne, numberTwo);
+}
+
+//? También con return numberOne > numberTwo ? numberOne : numberTwo;
+
+console.log(sum(3, 64));
