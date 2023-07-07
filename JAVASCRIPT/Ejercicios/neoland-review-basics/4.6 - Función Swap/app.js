ab6267c5d6ca180023e54263235994df759b1bca
guillebags <guillebags@gmail.com>
2023-07-07 17:28:35 +0200

    swap

---
+++ JAVASCRIPT/Ejercicios/neoland-review-basics/4.6 - Función Swap/app.js
@@ -0,0 +1,13 @@
+//! Iteración #6: Función swap
+//Crea una función llamada swap() que reciba un array y dos parametros que sean indices del array. La función deberá intercambiar la posición de los valores de los indices que hayamos enviado como parametro. Retorna el array resultante.
+
+const mandalorian = ["Grogu", "Mando", "Bo-Katan", "Boba Fett", "Kuiil"];
+
+const swap = (arr, i1, i2) => {
+  let save = arr[i1];
+  arr[i1] = arr[i2];
+  arr[i2] = save;
+  return arr;
+};
+
+console.log(swap(mandalorian, 3, 1));
