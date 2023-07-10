432393893f7477c74dc433a8d5b35602f90b6356
guillebags <guillebags@gmail.com>
2023-07-10 10:44:51 +0200

    filter even

--- JAVASCRIPT/Ejercicios/neoland-es6/5.5 - Filter/app.js
+++ JAVASCRIPT/Ejercicios/neoland-es6/5.5 - Filter/app.js
@@ -6,3 +6,9 @@ const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
 const adult = ages.filter((age) => age > 18);
 
 console.log(adult);
+
+//5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los valores que sean par.
+
+const evens = ages.filter((age) => age % 2 == 0);
+
+console.log(evens);
