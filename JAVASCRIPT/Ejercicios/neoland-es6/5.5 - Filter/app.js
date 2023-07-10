e94ed626f15b3831d0341836075fb31e6f20b649
guillebags <guillebags@gmail.com>
2023-07-10 16:19:38 +0200

    filter

--- JAVASCRIPT/Ejercicios/neoland-es6/5.5 - Filter/app.js
+++ JAVASCRIPT/Ejercicios/neoland-es6/5.5 - Filter/app.js
@@ -12,3 +12,40 @@ console.log(adult);
 const evens = ages.filter((age) => age % 2 == 0);
 
 console.log(evens);
+
+//5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+
+const streamers = [
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
+];
+
+const lolStreamers = streamers.filter(
+  (streamer) => streamer.gameMorePlayed == "League of Legends"
+);
+console.log(lolStreamers);
+
+//5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos usar la funcion .includes() para la comprobación.
+
+const uStreamers = streamers.filter((streamer) => streamer.name.includes("u"));
+
+console.log(uStreamers);
+
+//5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion .includes() para la comprobación. Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando .age sea mayor que 35.
+
+const legendsStreamers = streamers.filter((streamer) =>
+  streamer.gameMorePlayed.includes("Legends")
+);
+for (streamer of legendsStreamers) {
+  if (streamer.age > 35) {
+    streamer.gameMorePlayed = streamer.gameMorePlayed.toUpperCase();
+  }
+}
+
+console.log(legendsStreamers);
+
+//5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola los streamers que incluyan la palabra introducida en el input. De esta forma, si introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+
+//5.7 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola los streamers que incluyan la palabra introducida en el input. De esta forma, si introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.En este caso, muestra solo los streamers filtrados cuando hagamos click en el button.
