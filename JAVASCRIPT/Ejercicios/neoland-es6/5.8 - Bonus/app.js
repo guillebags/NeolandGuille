b9f3ca63e74cdd6af6406576814aeb6cbf4593a8
guillebags <guillebags@gmail.com>
2023-07-12 22:50:56 +0200

    ejercicio 2

--- JAVASCRIPT/Ejercicios/neoland-es6/5.8 - Bonus/app.js
+++ JAVASCRIPT/Ejercicios/neoland-es6/5.8 - Bonus/app.js
@@ -2,14 +2,18 @@
 //6.1 Dado el siguiente javascript filtra los videojuegos por gender = 'RPG' usando .filter() y usa .reduce() para conseguir la media de sus .score. La función .find() también podría ayudarte para el contrar el genero 'RPG' en el array .gender.
 
 const videogames = [
-  { name: "Final Fantasy VII", genders: ["RPG"], score: 9.5 },
-  { name: "Assasins Creed Valhala", genders: ["Aventura", "RPG"], score: 4.5 },
+  { name: "Greedfall", genders: ["RPG"], score: 9.5 },
+  {
+    name: "Assassins Creed Valhalla",
+    genders: ["Aventura", "RPG"],
+    score: 6.5,
+  },
   { name: "The last of Us 2", genders: ["Acción", "Aventura"], score: 9.8 },
   { name: "Super Mario Bros", genders: ["Plataforma"], score: 8.5 },
   { name: "Genshin Impact", genders: ["RPG", "Aventura"], score: 7.5 },
   {
-    name: "Legend of Zelda: Breath of the wild",
-    genders: ["RPG", "La cosa más puto bonita que he visto nunca"],
+    name: "Hogwarts Legacy",
+    genders: ["RPG", "La cosa más bonita que he visto nunca"],
     score: 10,
   },
 ];
