da41cb3bdbcc195d7641302f72045ed6c9b9a5e9
guillebags <guillebags@gmail.com>
2023-07-18 13:35:10 +0200

    apuntes

---
+++ JAVASCRIPT/Apuntes/javascript/apuntesdom/main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const aplication = document.querySelector("#app");
+
+const classSelect = document.querySelector(".clase");
+
+const imgSelect = document.getElementsByTagName("img");
