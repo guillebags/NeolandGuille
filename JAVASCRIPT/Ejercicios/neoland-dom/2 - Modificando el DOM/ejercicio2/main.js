ff437acd36205736b820ae795a2a16cfe6261e84
guillebags <guillebags@gmail.com>
2023-07-19 18:25:33 +0200

    neoland-dom hecho

--- JAVASCRIPT/Ejercicios/neoland-dom/2 - Modificando el DOM/ejercicio2/main.js
+++ JAVASCRIPT/Ejercicios/neoland-dom/2 - Modificando el DOM/ejercicio2/main.js
@@ -27,7 +27,41 @@ main.appendChild(p);
 
 //2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
 
-const textoH2 = document.createTextNode("Wubba Lubba dub dub");
-const h2 = document.h2.getElementsByClassName("fn-insert-here");
+const h2 = document.querySelector(".fn-insert-here");
 
-h2.innerText += "Wubba Lubba dub dub";
+h2.innerText = "Wubba Lubba dub dub";
+
+//2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
+
+const apps = ["Facebook", "Netflix", "Instagram", "Snapchat", "Twitter"];
+
+const ul = document.createElement("ul");
+
+main.appendChild(ul);
+
+for (let i = 0; i < apps.length; i++) {
+  const li = document.createElement("li");
+  li.innerText = apps[i];
+  ul.appendChild(li);
+}
+
+//2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
+
+const removedItems = document.getElementsByClassName("fn-remove-me");
+
+while (removedItems.length > 0) {
+  main.removeChild(removedItems[0]);
+}
+
+//2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. Recuerda que no solo puedes insertar elementos con .appendChild.
+
+const divIns = document.querySelector("div");
+divIns.insertAdjacentHTML("afterend", "<p>Voy en medio!</p>");
+
+//2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here
+
+let allDivs = document.querySelectorAll("div.fn-insert-here");
+
+for (let i = 0; i < allDivs.length; i++) {
+  allDivs[i].innerHTML = "<p>Voy dentro!</p>";
+}
