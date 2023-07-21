0ddffcc0742c61fc7bed340af259cd9321c20255
guillebags <guillebags@gmail.com>
2023-07-21 09:26:19 +0200

    async

--- JAVASCRIPT/Ejercicios/neoland-asynchronous/async/main.js
+++ JAVASCRIPT/Ejercicios/neoland-asynchronous/async/main.js
@@ -20,27 +20,34 @@ const getData = async () => {
 // a la api que diga...'El nombre X tiene un Y porciento de ser de Z' etc etc.
 // EJ: El nombre Pepe tiene un 22 porciento de ser de ET.
 
-const urlNation = "https://api.nationalize.io";
+//!1.4
+//En base al ejercicio anterior, crea un botón con el texto 'X' para cada uno de los p que hayas insertado y que si el usuario hace click en este botón eliminemos el parrafo asociado.
 
+//?selectores
 const button = document.querySelector("button");
 
+//?creo h2 en el elemento para pintar el total de personas con ese nombre
 const h2 = document.createElement("h2");
+const clearH2 = () => (h2.innerText = "");
+const urlNation = "https://api.nationalize.io";
+const printButton = document.createElement("button");
 
 button.addEventListener("click", async () => {
   const value = document.querySelector("input").value;
   const dataNation = await getDataNation(urlNation, value);
   const elements = dataNation.country.map(({ country_id, probability }) => {
+    //mapeo el array de paises para sacar un li con el texto por cada uno
     const li = document.createElement("li");
+    li.append(printButton);
     li.innerText = `El nombre ${dataNation.name} tiene un ${
       probability * 100
     } porciento de ser de ${country_id}`;
+
     return li;
   });
-
-  h2.innerText = "";
+  clearH2();
   h2.innerText = `Total ${dataNation.count}`;
   document.body.append(h2);
-
   document.querySelector("ul").replaceChildren(...elements);
 });
 
