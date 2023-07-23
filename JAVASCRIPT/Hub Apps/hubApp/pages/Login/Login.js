5a13f44707e754c2f1cde4fc3c0cb88ff06fa759
guillebags <guillebags@gmail.com>
2023-07-23 21:34:17 +0200

    login maquetado

--- JAVASCRIPT/Hub Apps/hubApp/pages/Login/Login.js
+++ JAVASCRIPT/Hub Apps/hubApp/pages/Login/Login.js
@@ -1,12 +1,29 @@
 import { setUser } from "../../Global/state/globalState";
 import { initControler } from "../../utils/route";
+import "./Login.css/";
 
 const template = () => `
-  <div>
-    <h1>Login</h1>
-    <input type="text" name="username" id="username" />
-    <button id="buttonLogin">enviar</button>
+<div class="container" id="containerLogin">
+<div class="screen">
+  <div class="screen__content">
+    <form class="login">
+      <div class="login__field">
+        <i class="login__icon fas fa-user"></i>
+        <input type="text" name="username" id="username" placeholder="User Name"/>
+
+      </div>
+      <button class="button login__submit" id = "buttonLogin" >
+        <span class="button__text">Log In Now</span>
+        <i class="button__icon fas fa-chevron-right"></i>
+      </button>				
+    </form>
   </div>
+  <div class="screen__background">
+    <img src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/a/a5/latest/20160604220739/Rotom.png/200px-Rotom.png" alt="rotom login" class="screen__background__shape" id="rotomLogin">
+    <span class="screen__background__shape screen__background__shape1"></span>
+  </div>		
+</div>
+</div>
 `;
 
 const addListeners = () => {
