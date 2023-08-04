e17fd935c400c70992365275c6ad6772389663ba
guillebags <guillebags@gmail.com>
2023-08-04 15:16:55 +0200

    ignore

--- NODE/Proyecto Node/api/models/User.model.js
+++ NODE/Proyecto Node/api/models/User.model.js
@@ -2,26 +2,39 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const validator = require("validator");
 
-const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-    validate: [validator.isEmail, "Not a valid email"],
+//creamos el modelo de user
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      validate: [validator.isEmail, "Not a valid email"],
+    },
+    name: { type: String, required: true, trim: true, unique: true },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: [validator.isStrongPassword],
+      minlength: [8, "Min 8 characters"],
+    },
+    gender: {
+      type: String,
+      enum: ["mujer", "hombre", "no binarie", "otros"],
+      required: true,
+    },
+    rol: { type: String, enum: ["admin", "user"], required: true },
+    confirmationCode: { type: Number, required: true },
+    check: { type: Boolean, default: false },
+    image: { type: String },
   },
-  name: { type: String, required: true, trim: true, unique: true },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    validate: [validator.isStrongPassword],
-    minlength: [8, "Min 8 characters"],
-  },
-  gender: {
-    type: String,
-    enum: ["mujer", "hombre", "no binarie", "otros"],
-    required: true,
-  },
-  rol: { type: String, enum: ["admin", "user"], required: true },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next) {
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+  } catch (error) {}
 });
