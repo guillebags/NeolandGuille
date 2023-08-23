635cfd56d58bcccf770f07ea657975de2259cf93
guillebags <guillebags@gmail.com>
2023-08-23 11:37:20 +0200

    apuntes

--- REACT/Ejercicios/Proyecto Ejercicio/proyectoejercicio/src/App.jsx
+++ REACT/Ejercicios/Proyecto Ejercicio/proyectoejercicio/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Header from "./components/UI/Header/Header";
 import Footer from "./components/UI/Footer/Footer";
 import Main from "./components/UI/Main/Main";
+import { UserContext } from "./context/userContextAuth";
+import { useContext } from "react";
 
 const App = () => {
   return (
