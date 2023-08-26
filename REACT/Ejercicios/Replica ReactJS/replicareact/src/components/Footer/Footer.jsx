2cbf1c4c34d8a5f25a2dd97bc101534dd324668c
guillebags <guillebags@gmail.com>
2023-08-26 21:15:32 +0200

    footer terminado

--- REACT/Ejercicios/Replica ReactJS/replicareact/src/components/Footer/Footer.jsx
+++ REACT/Ejercicios/Replica ReactJS/replicareact/src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import "./Footer.css";
+import FooterIcons from "./FooterIcons";
 import FooterLinks from "./FooterLinks";
 
 export const Footer = () => {
   return (
     <footer>
       <FooterLinks />
+      <FooterIcons />
     </footer>
   );
 };
